a1d8467ce5e42be8fa6442a263330556ae9b24d6
Kevin Quinn <kevin@kevin-laptop.(none)>
2011-03-22 20:01:19 -0400

    Added google map API key for development version at: swarm-alpha3.heroku.com

--- config/initializers/app_constants.rb
+++ config/initializers/app_constants.rb
@@ -3,7 +3,7 @@ module AppConstants
   GOOGLE_API_KEY = if Rails.env.development? || Rails.env.test?
     "ABQIAAAAMC7dq9bWcPaJdGxWPDaq1BTJQa0g3IQ9GZqIMmInSLzwtGDKaBQMJsqyuhJOmM2r4r_WiYnAjVdF8g"
   else # production
-    "ABQIAAAAMC7dq9bWcPaJdGxWPDaq1BTJQa0g3IQ9GZqIMmInSLzwtGDKaBQMJsqyuhJOmM2r4r_WiYnAjVdF8g"
+    "ABQIAAAAMC7dq9bWcPaJdGxWPDaq1BTVSS5_ibbxvSbvjJtZctKVafn8shT0y6XNeZKdsH46ysYq3agOwv3VnA"
   end
   
 end
