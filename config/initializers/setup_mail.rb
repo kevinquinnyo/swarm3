074d092d051e22d63ca5aa85c0d0620ae8b1b527
Kevin Quinn <kevin@kevin-laptop.(none)>
2011-04-05 19:47:23 -0400

    password incorrect in smtp settings, hopefully thats the last problem?

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
 :port                   => 587,
 :domain                 => "gmail.com",
 :user_name              => "swarmdelivery@gmail.com",
-:password               => "kqtw1716K",
+:password               => "kqtw1716",
 :authentication         => "plain",
 :enable_starttls_auto   => true
 }
