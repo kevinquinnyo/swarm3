0704f2f1fa469a16f7b12b3ea68b074cbc11312f
Kevin Quinn <kevin@kevin-laptop.(none)>
2011-04-06 16:01:12 -0400

    ip geocode testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  helper :all
+  geocode_ip_address
+  
+  before_filter :geokit
+
+  def geokit 
+    store_ip_location
+    @current_location = session[:geo_location]  # @current_location is a GeoLoc instance. 
+  end
+  def store_location
+    session[:return_to] = request.request_uri
+  end
+  
+end
+
   def require_admin!
     unless current_user.try :is_admin?
       redirect_to root_path, :alert => "Access Denied."
