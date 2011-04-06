fd6104932090e4cf86c6eb1468ae6518ed00fe45
Kevin Quinn <kevin@kevin-laptop.(none)>
2011-04-05 20:09:01 -0400

    forgot one more thing, needed new google api key with the name change

--- config/initializers/app_constants.rb
+++ config/initializers/app_constants.rb
@@ -4,7 +4,7 @@ module AppConstants
   GOOGLE_API_KEY = 
     "ABQIAAAAMC7dq9bWcPaJdGxWPDaq1BTJQa0g3IQ9GZqIMmInSLzwtGDKaBQMJsqyuhJOmM2r4r_WiYnAjVdF8g"
   else # production
-    "ABQIAAAAMC7dq9bWcPaJdGxWPDaq1BTVSS5_ibbxvSbvjJtZctKVafn8shT0y6XNeZKdsH46ysYq3agOwv3VnA"
+    "ABQIAAAA3o0gpoxhBa8wt07axEnPEhSFtz6H3hWtsjLlk5R7ntmycOL6mhTPErQqRhAO8dr_JO8U3PVJSClDqg"
   end
   
 end
