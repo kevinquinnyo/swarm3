6260dfd014534184e3676963e2dfaeb27fbd874f
Kevin Quinn <kevin@kevin-laptop.(none)>
2011-04-10 21:12:19 -0400

    fixed syntax error in migration

--- db/migrate/20110410204348_add_delivery_count_to_users.rb
+++ db/migrate/20110410204348_add_delivery_count_to_users.rb
@@ -1,6 +1,6 @@
 class AddDeliveryCountToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :delivery_count, :integer :default => 0
+    add_column :users, :delivery_count, :integer, :default => 0
   end 
 
   def self.down
